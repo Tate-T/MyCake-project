1c11ffc965ddca35537b5466c58cffd4bad1ef39
Devohka <andr.fedak@gmail.com>
2024-09-13 22:39:22 +0300

    cxv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Confecioners } from "./pages/Confecioners/Confecioners";
+// import { Confecioners } from "./pages/Confecioners/Confecioners";
 // import { Main } from "./pages/main/Main";
-
+import { Desinger } from "./components/general-components/modal/Designer";
 function App() {
   return (
     <>
-      <Confecioners />
+      <Desinger />
     </>
   )
 }
