fb93cf740336b91e036f72cd0607a0b9a9986164
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-12-21 10:09:16 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import { useState } from 'react'
-import Modal1 from './components/loginModals/Modal1'
-import Modal2 from './components/loginModals/Modal2'
-import Modal3 from './components/loginModals/Modal3'
-import ModalResp from './components/loginModals/ModalResp'
-import AboutConfectioner from './pages/AboutConfectioner/AboutConfectioner'
-import { Main } from './pages/main/Main'
-import { Route, Routes } from 'react-router-dom'
-import { Desserts } from './pages/Desserts/desserts'
-import {Confecioners} from "./pages/Confecioners/Confecioners"
-import Articles from './pages/Articles'
-import InfoConfecoiners from './pages/InfoConfecioners/InfoConfecioners'
-import ConfectionerProducts from './pages/ConfectionerProducts/ConfectionerProducts'
+import { useState } from "react";
+import Modal1 from "./components/loginModals/Modal1";
+import Modal2 from "./components/loginModals/Modal2";
+import Modal3 from "./components/loginModals/Modal3";
+import ModalResp from "./components/loginModals/ModalResp";
+import aboutConfectioner from "./pages/AboutConfectioner/AboutConfectioner";
+import { Main } from "./pages/main/Main";
+import { Route, Routes } from "react-router-dom";
+import { Desserts } from "./pages/Desserts/desserts";
+import { Confecioners } from "./pages/Confecioners/Confecioners";
+import Articles from "./pages/Articles";
+import { ArticlePage } from "./pages/Article/Article";
+import InfoConfecoiners from "./pages/InfoConfecioners/InfoConfecioners";
 function App() {
-	const [loginModal, setLoginModal] = useState(false)
-	const [secondModal, setSecondModal] = useState('')
-	const [verificationModal, setVerificationModal] = useState({
-		hidden: false,
-		type: false,
-	})
-	const pack = [setLoginModal, setSecondModal, setVerificationModal]
-	return (
-		<>
-			<Routes>
-      <Route path="/" element={<Main />}></Route>
-				<Route path="/desserts" element={<Desserts />}></Route>
+  const [loginModal, setLoginModal] = useState(false);
+  const [secondModal, setSecondModal] = useState("");
+  const [verificationModal, setVerificationModal] = useState({
+    hidden: false,
+    type: false,
+  });
+  const pack = [setLoginModal, setSecondModal, setVerificationModal];
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/desserts" element={<Desserts />}></Route>
         <Route path="/confecionres" element={<Confecioners />}></Route>
         <Route path="/articles" element={<Articles />}></Route>
+        <Route path="/articles/:id" element={<ArticlePage />} />
         <Route path="/infoConfecoiners" element={<InfoConfecoiners />}></Route>
-		<Route path="/confectioner">
-			<Route path="products" element={<ConfectionerProducts />}></Route>
-			<Route path="about" element={<AboutConfectioner />}></Route>
-		</Route>
-			</Routes>
+      </Routes>
 
-			{/* <ModalResp/>
+      {/* <ModalResp/>
 			<Main func={setLoginModal} />
 			{loginModal ? <Modal1 obj={pack} /> : <></>}
 			{secondModal === 'login' ? <Modal2 obj={pack} check="login" /> : <></>}
@@ -51,8 +48,8 @@ function App() {
 				<></>
 			)}
 			{console.log(verificationModal)} */}
-		</>
-	)
+    </>
+  );
 }
 
-export default App
+export default App;
